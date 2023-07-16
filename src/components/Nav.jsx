9e6a27134e8a97e48a904eb87984965631b1daf1
Chakri <chakradhar@addant.com>
2023-07-16 19:50:07 +0530

    wise

---
+++ src/components/Nav.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='container-fluid'>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src='https://imgur.com/U61yxOl.png' className='img-fluid'/>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">Doc</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Invests</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Socials</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
