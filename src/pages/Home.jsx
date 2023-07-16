bd585289be94bc7a34fb23d334429588b2194c2b
Christal C J <christal@addant.com>
2023-07-17 02:27:48 +0530

    carouselempss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,20 @@ import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
 import Section4 from '../components/Section4'
 import Section5 from '../components/Section5'
+import usePreventBodyScroll from '../components/UsePreventBodyScroll'
 
 export default function Home() {
+  const { disableScroll, enableScroll } = usePreventBodyScroll();
   return (
     <div className='px-lg-4'>
         <Nav/>
-        
         <Banner/>
         <Section1/>
         <Section2/>
         <Section3/>
+        <div onMouseEnter={disableScroll} onMouseLeave={enableScroll}>
         <Pathtomarket/>
+        </div>
         <Section4/>
         <Section5/>
     </div>
