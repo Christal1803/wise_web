9b90b2563a07520fbd64ff0e38a563e334eb2149
Chakri <chakradhar@addant.com>
2023-07-17 02:29:38 +0530

    dfgh

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -17,43 +17,100 @@ export default function Section4() {
 
             <div className='col-12 mt-5'>
                 {/* slider */}
-                <OwlCarousel className='owl-theme' items={3} autoplay={true} center={true} dots={true} loop={true} nav>
-    <div class='item'>
-        <h4>1</h4>
+                <OwlCarousel className='owl-theme' items={3} autoplay={false} center={true} dots={true} loop={true} nav>
+   
+   
+
+
+
+
+    <div class='item row '>
+
+
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+    <p className='user-name'>Dhivya Daniel (DD)</p>
+    <p className='user-role'>CEO</p>
+    <p className='user-bio'>Steered MetaWise Marketing as CEO10+ years of industry experiencePGDM - Marketing (LIBA)Keynote SpeakerData-driven decision-makerLocation: Munich, Germany</p>
+</div>
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+<div className='owl-img'>
+    <img src='https://imgur.com/FCuEGoA.png' className='img-fluid'/>
+</div></div>
+
+</div>
+
+
+
+<div class='item row '>
+
+
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+    <p className='user-name'>Livingston Cruz</p>
+    <p className='user-role'>COO</p>
+    <p className='user-bio'>Led Enterprise level Digital Transformations12 years in SaaS development & international rolloutsPGDM - Marketing, LIBAGood Storyteller, People-HandlerLocation: Munich, Germany</p>
+</div>
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+<div className='owl-img'>
+    <img src='https://imgur.com/Up8aDy6.png' className='img-fluid'/>
+</div>
+</div>
+
+</div>
+
+
+
+<div class='item row '>
+
+
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+    <p className='user-name'>Sam Prabhu</p>
+    <p className='user-role'>CPO</p>
+    <p className='user-bio'>7-year SimScale StalwartM.Sc., RWTH Aachen UniversityStartup Ecosystem Expertise & Client Success/Growth StrategistAI Enthusiast, Chess AficionadoLocation: Munich, Germany</p>
+</div>
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+<div className='owl-img'>
+    <img src='https://imgur.com/3VvcAC3.png' className='img-fluid'/>
+</div></div>
+
+</div>
+
+
+
+<div class='item row '>
+
+
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+    <p className='user-name'>Shreyan Mehta</p>
+    <p className='user-role'>CTO</p>
+    <p className='user-bio'>10+ years of domain experience4+ years AI & GPT6+ years MERN stack5+ years as a Scrum MasterLaunched “SitesGPT” & “loveGPT"Location: Ahmedabad, India</p>
+</div>
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+<div className='owl-img'>
+
+    <img src='https://imgur.com/wYaaEe9.png' className='img-fluid'/>
+</div></div>
+
+</div>
+
+
+
+<div class='item row '>
+
+
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+    <p className='user-name'>Mithilesh Nanj</p>
+    <p className='user-role'>Data Scientist</p>
+    <p className='user-bio'>7-year of Data Science expM.S.Data Science, Indiana University Bloomingtonex- YahooLocation: Illinois, USA</p>
+</div>
+<div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+    <div className='owl-img'>
+    <img src='https://imgur.com/bH8TD4B.png' className='img-fluid'/>
     </div>
-    <div class='item'>
-        <h4>2</h4>
-    </div>
-    <div class='item'>
-        <h4>3</h4>
-    </div>
-    <div class='item'>
-        <h4>4</h4>
-    </div>
-    <div class='item'>
-        <h4>5</h4>
-    </div>
-    <div class='item'>
-        <h4>6</h4>
-    </div>
-    <div class='item'>
-        <h4>7</h4>
-    </div>
-    <div class='item'>
-        <h4>8</h4>
-    </div>
-    <div class='item'>
-        <h4>9</h4>
-    </div>
-    <div class='item'>
-        <h4>10</h4>
-    </div>
-    <div class='item'>
-        <h4>11</h4>
-    </div>
-    <div class='item'>
-        <h4>12</h4>
-    </div>
+    
+</div>
+
+</div>
+
 </OwlCarousel>;
             </div>
             
