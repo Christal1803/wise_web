7a6e62942e3f5ede27e3a70449b6b5c47b7cf601
Christal C J <christal@addant.com>
2023-07-17 00:10:47 +0530

    carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Pathtomarket from '../components/Pathtomarket'
 
 export default function Home() {
   return (
     <div>
         <Nav/>
+        <Pathtomarket/>
     </div>
   )
 }
