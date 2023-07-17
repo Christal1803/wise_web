7e0d8e977bf8a4dec6c9dbf9018be6243ab6d163
Chakri <chakradhar@addant.com>
2023-07-17 20:54:47 +0530

    sdas

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,9 @@ import twitter from '../assets/twitter.png'
 import linkedin from '../assets/linkedin.png'
 import facebook from '../assets/fb.png'
 import insta from '../assets/insta.png'
+import yt from '../assets/youtube.svg'
+import mail from '../assets/gmail.svg'
+import threads from '../assets/threads.svg'
 
 export default function Nav() {
 
@@ -56,11 +59,11 @@ export default function Nav() {
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="https://twitter.com/theWise_AI"><img src={twitter} className="img-fluid me-2"/>Twitter</a></li>
             <li><a class="dropdown-item" href="https://www.linkedin.com/company/thewise-ai/"><img src={linkedin} className="img-fluid me-2"/>LinkedIn</a></li>
-            <li><a class="dropdown-item" href="https://www.linkedin.com/company/thewise-ai/"><img src={facebook} className="img-fluid me-2"/>Facrbook</a></li>
-            <li><a class="dropdown-item" href="https://www.threads.net/@thewise_ai"><img src={facebook} className="img-fluid me-2"/>Threads</a></li>
-            <li><a class="dropdown-item" href="https://www.youtube.com/@theWise_AI/"><img src={facebook} className="img-fluid me-2"/>Youtube</a></li>
+            <li><a class="dropdown-item" href="https://www.linkedin.com/company/thewise-ai/"><img src={facebook} className="img-fluid me-2"/>Facebook</a></li>
+            <li><a class="dropdown-item" href="https://www.threads.net/@thewise_ai"><img src={threads} className="img-fluid me-2 f-icon"/>Threads</a></li>
+            <li><a class="dropdown-item" href="https://www.youtube.com/@theWise_AI/"><img src={yt} className="img-fluid me-2 f-icon"/>Youtube</a></li>
             <li><a class="dropdown-item" href="https://www.instagram.com/thewise_ai/"><img src={insta} className="img-fluid me-2"/>Instagram</a></li>
-            <li><a class="dropdown-item" href="mailto:contact@thewise.ai"><img src={facebook} className="img-fluid me-2"/>Email</a></li>
+            <li><a class="dropdown-item" href="mailto:contact@thewise.ai"><img src={mail} className="img-fluid me-2 f-icon"/>Email</a></li>
           </ul>
         </li>
 
