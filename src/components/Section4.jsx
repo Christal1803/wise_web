12649cda431bbf93d11515feab285ef28d252901
Sasidharan M <sasidharan.itprof@gmail.com>
2023-07-18 10:58:00 +0530

    Carousel fixed

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import masters from '../assets/masters.png'
-import Dhivya from '../assets/Dhivya.png'
-import Livingston from '../assets/Livingston.png'
-import SamPrabhu from '../assets/SamPrabhu.png'
-import Shreyan from '../assets/Shreyan.png'
-import Mithilesh from '../assets/Mithilesh.png'
-import cardlink from '../assets/card-link.png'
+import masters from "../assets/masters.png";
+import Dhivya from "../assets/Dhivya.png";
+import Livingston from "../assets/Livingston.png";
+import SamPrabhu from "../assets/SamPrabhu.png";
+import Shreyan from "../assets/Shreyan.png";
+import Mithilesh from "../assets/Mithilesh.png";
+import cardlink from "../assets/card-link.png";
 
 export default function Section4() {
   return (
@@ -16,11 +16,7 @@ export default function Section4() {
       <div className="row">
         <div className="col-12">
           <div className="section1 text-center">
-            <img
-              src={masters}
-              className="img-fluid"
-              alt="Section 4 Logo"
-            />
+            <img src={masters} className="img-fluid" alt="Section 4 Logo" />
             <p className="section1-head">Meet the Masters Behind the Magic</p>
             <p className="section1-text">
               A Team of Visionaries Driving Innovation at thewise.ai
@@ -58,7 +54,20 @@ export default function Section4() {
           >
             <div class="row my-3">
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto my-content">
-                <p className="user-name">Dhivya Daniel (DD) <a href="https://www.linkedin.com/in/dhivyadaniel/" target="_blank"><img src={cardlink} className="ms-1 card-linked img-fluid"/></a></p>
+                <p className="user-name">
+                  Dhivya Daniel (DD){" "}
+                  <a
+                    href="https://www.linkedin.com/in/dhivyadaniel/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={cardlink}
+                      className="ms-1 card-linked img-fluid"
+                      alt="CEO"
+                    />
+                  </a>
+                </p>
                 <p className="user-role">CEO</p>
                 <p className="user-bio">
                   <div>Steered MetaWise Marketing as CEO</div>
@@ -71,85 +80,131 @@ export default function Section4() {
               </div>
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto">
                 <div className="owl-img">
-                  <img
-                    src={Dhivya}
-                    className="img-fluid d-inline"
-                    alt="CEO"
-                  />
+                  <img src={Dhivya} className="img-fluid d-inline" alt="CEO" />
                 </div>
               </div>
             </div>
 
             <div class="row my-3">
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto my-content">
-                <p className="user-name">Livingston Cruz <a href="https://www.linkedin.com/in/cruz-livingston/" target="_blank"><img src={cardlink} className="ms-1 card-linked img-fluid"/></a></p>
+                <p className="user-name">
+                  Livingston Cruz{" "}
+                  <a
+                    href="https://www.linkedin.com/in/cruz-livingston/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={cardlink}
+                      className="ms-1 card-linked img-fluid"
+                      alt="COO"
+                    />
+                  </a>
+                </p>
                 <p className="user-role">COO</p>
                 <p className="user-bio">
-                  Led Enterprise level Digital Transformations12 years in SaaS
-                  development & international rolloutsPGDM - Marketing, LIBAGood
-                  Storyteller, People-Handler Location: Munich, Germany
+                  <div>Led Enterprise level Digital Transformations</div>
+                  <div>12 years in SaaS product development & international rollouts</div>
+                  <div>PGDM - Marketing, LIBA</div>
+                  <div>Good Storyteller, People-Handler</div>
+                  <div>Location: Munich, Germany</div>
                 </p>
               </div>
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto">
                 <div className="owl-img">
-                  <img
-                    src={Livingston}
-                    className="img-fluid"
-                    alt="COO"
-                  />
+                  <img src={Livingston} className="img-fluid" alt="COO" />
                 </div>
               </div>
             </div>
 
             <div class="row my-3">
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto my-content">
-                <p className="user-name">Sam Prabhu <a href="https://www.linkedin.com/in/samprabhuj/" target="_blank"><img src={cardlink} className="ms-1 card-linked img-fluid"/></a></p>
+                <p className="user-name">
+                  Sam Prabhu{" "}
+                  <a
+                    href="https://www.linkedin.com/in/samprabhuj/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={cardlink}
+                      className="ms-1 card-linked img-fluid"
+                      alt="CPO"
+                    />
+                  </a>
+                </p>
                 <p className="user-role">CPO</p>
                 <p className="user-bio">
-                  7-year SimScale StalwartM.Sc., RWTH Aachen UniversityStartup
-                  Ecosystem Expertise & Client Success/Growth StrategistAI
-                  Enthusiast, Chess Aficionado Location: Munich, Germany
+                  <div>7-year SimScale Stalwart</div>
+                  <div>M.Sc., RWTH Aachen University</div>
+                  <div>Startup Ecosystem Expertise & Client Success/Growth Strategist</div>
+                  <div>Chess Aficionado</div>
+                  <div>Location: Munich, Germany</div>
                 </p>
               </div>
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto">
                 <div className="owl-img">
-                  <img
-                    src={SamPrabhu}
-                    className="img-fluid"
-                    alt="CPO"
-                  />
+                  <img src={SamPrabhu} className="img-fluid" alt="CPO" />
                 </div>
               </div>
             </div>
 
             <div class="row my-3">
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto my-content">
-                <p className="user-name">Shreyan Mehta <a href="https://www.linkedin.com/in/devsm/" target="_blank"><img src={cardlink} className="ms-1 card-linked img-fluid"/></a></p>
+                <p className="user-name">
+                  Shreyan Mehta{" "}
+                  <a
+                    href="https://www.linkedin.com/in/devsm/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={cardlink}
+                      className="ms-1 card-linked img-fluid"
+                      alt="CTO"
+                    />
+                  </a>
+                </p>
                 <p className="user-role">CTO</p>
                 <p className="user-bio">
-                  10+ years of domain experience4+ years AI & GPT6+ years MERN
-                  stack5+ years as a Scrum MasterLaunched “SitesGPT” &
-                  “loveGPT" Location: Ahmedabad, India
+                  <div>10+ years of domain experience</div>
+                  <div>4+ years AI & GPT</div>
+                  <div>6+ years MERN stack</div>
+                  <div>5+ years as a Scrum Master</div>
+                  <div>Launched “SitesGPT” & “loveGPT"</div>
+                  <div>Location: Ahmedabad, India</div>
                 </p>
               </div>
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto">
                 <div className="owl-img">
-                  <img
-                    src={Shreyan}
-                    className="img-fluid"
-                    alt="CTO"
-                  />
+                  <img src={Shreyan} className="img-fluid" alt="CTO" />
                 </div>
               </div>
             </div>
 
             <div class="row my-3">
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto my-content">
-                <p className="user-name">Mithilesh Nanj<a href="https://www.linkedin.com/in/mithileshnanj/" target="_blank"><img src={cardlink} className="ms-1 card-linked img-fluid"/></a></p>
+                <p className="user-name">
+                  Mithilesh Nanj
+                  <a
+                    href="https://www.linkedin.com/in/mithileshnanj/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={cardlink}
+                      className="ms-1 card-linked img-fluid"
+                      alt="Data"
+                    />
+                  </a>
+                </p>
                 <p className="user-role">Data Scientist</p>
                 <p className="user-bio">
-                  7-year of Data Science exp, M.S.Data Science, Indiana University
-                  Bloomingtonex- Yahoo Location: Illinois, USA
+                  <div>7-year of Data Science exp</div>
+                  <div>M.S.Data Science</div>
+                  <div>Indiana University Bloomington</div>
+                  <div>Ex- Yahoo</div>
+                  <div>Location: Illinois, USA</div>
                 </p>
               </div>
               <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 my-auto">
